97304aff133723a4f2bcedab27a0ed7954fcac2a
Arina2707 <arina.odnoblyudova@mail.ru>
2021-04-20 12:37:01 +0300

    commit

--- bayesnet.py
+++ bayesnet.py
@@ -260,32 +260,21 @@ class CustomerBayesNet(BayesNet):
         super(CustomerBayesNet, self).__init__()
 
     @staticmethod
-    def add_first_layer(e1, w1, e2, w2, e3, w3, dt, ap):
+    def add_first_layer(e1, w1, e2, w2, e3, w3, dt):
         mensions = DiscreteDistribution({'yes': e1, 'no': 1 - e1})
         positiveness = DiscreteDistribution({'yes': w1, 'no': 1 - w1})
-        subscribers = DiscreteDistribution({'yes': e2, 'no': 1 - e2})
+        percentage = DiscreteDistribution({'yes': e2, 'no': 1 - e2})
 
         overall_rank = DiscreteDistribution({'yes': w2, 'no': 1 - w2})
         reach_rank = DiscreteDistribution({'yes': e3, 'no': 1 - e3})
         rank_per_mill = DiscreteDistribution({'yes': w3, 'no': 1 - w3})
 
         views_rank = DiscreteDistribution({'yes': dt, 'no': 1 - dt})
-        views_per_user = DiscreteDistribution({'yes': ap, 'no': 1 - ap})
 
-        return [mensions, positiveness, subscribers, overall_rank, reach_rank, rank_per_mill, views_rank, views_per_user]
+        return [mensions, positiveness, percentage, overall_rank, reach_rank, rank_per_mill, views_rank]
 
     @staticmethod
     def create_conditional_probs(l):
-        views = ConditionalProbabilityTable(
-            [['yes', 'no', 'yes', 0.67],
-             ['no', 'yes', 'yes', 0.33],
-             ['yes', 'yes','yes', 1.0],
-             ['no', 'no','yes', 0.0],
-             ['yes', 'no', 'no', 0.33],
-             ['no', 'yes', 'no', 0.67],
-             ['yes', 'yes', 'no', 0.0],
-             ['no', 'no', 'no', 1.0]], [l[6], l[7]])
-
 
         rank = ConditionalProbabilityTable(
             [['yes', 'no', 'yes', 'yes', 0.33],
@@ -331,7 +320,7 @@ class CustomerBayesNet(BayesNet):
              ['yes', 'no', 'no', 0.67],
              ['no', 'yes', 'no', 0.33],
              ['yes', 'yes', 'no', 0.0],
-             ['no', 'no', 'no', 1.0]], [rank, views])
+             ['no', 'no', 'no', 1.0]], [rank, l[6]])
 
         overall = ConditionalProbabilityTable(
             [['yes', 'no', 'yes', 0.67],
@@ -343,42 +332,39 @@ class CustomerBayesNet(BayesNet):
              ['yes', 'yes', 'no', 0.0],
              ['no', 'no', 'no', 1.0]], [reviews, website])
 
-        return [views, rank, reviews, website, overall]
+        return [rank, reviews, website, overall]
 
-    def bake_network(self, e1, w1, e2, w2, e3, w3, dt, ap):
-        nodes1 = self.add_first_layer(e1, w1, e2, w2, e3, w3, dt, ap)
+    def bake_network(self, e1, w1, e2, w2, e3, w3, dt):
+        nodes1 = self.add_first_layer(e1, w1, e2, w2, e3, w3, dt)
         nodes2 = self.create_conditional_probs(nodes1)
         n = nodes1 + nodes2
 
         s1 = State(n[0], name="mensions")
         s2 = State(n[1], name="positiveness")
-        s3 = State(n[2], name="subscribers")
+        s3 = State(n[2], name="percentage")
         s4 = State(n[3], name="overall_rank")
         s5 = State(n[4], name="reach_rank")
         s6 = State(n[5], name="rank_per_mill")
         s7 = State(n[6], name="views_rank")
-        s8 = State(n[7], name="views_per_user")
-        s9 = State(n[8], name="views")
-        s10 = State(n[9], name="rank")
-        s11 = State(n[10], name="reviews")
-        s12 = State(n[11], name="website")
-        s13 = State(n[13], name="Overall")
+        s8 = State(n[9], name="rank")
+        s9 = State(n[10], name="reviews")
+        s10 = State(n[11], name="website")
+        s11 = State(n[13], name="Overall")
 
-        self.network.add_states(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13)
-        self.network.add_edge(s1, s11)
-        self.network.add_edge(s2, s11)
-        self.network.add_edge(s3, s11)
-
+        self.network.add_states(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11)
+        self.network.add_edge(s1, s9)
+        self.network.add_edge(s2, s9)
         self.network.add_edge(s3, s9)
-        self.network.add_edge(s4, s9)
-        self.network.add_edge(s5, s9)
-        self.network.add_edge(s6, s8)
-        self.network.add_edge(s7, s8)
 
-        self.network.add_edge(s8, s11)
+        self.network.add_edge(s4, s8)
+        self.network.add_edge(s5, s8)
+        self.network.add_edge(s6, s8)
+        self.network.add_edge(s7, s10)
+        self.network.add_edge(s8, s10)
+
         self.network.add_edge(s9, s11)
-        self.network.add_edge(s10, s13)
-        self.network.add_edge(s11, s13)
+        self.network.add_edge(s10, s11)
 
         self.network.bake()
+
 
--- main.py
+++ main.py
@@ -9,15 +9,20 @@ warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 
 def make_net_scores():
-    data = pd.read_excel(r'C:\Users\maxim\OneDrive\Desktop\folder\diplom\data\parsing\final_companies.xlsx')
+    data = pd.read_excel(r'C:\Users\maxim\OneDrive\Desktop\folder\diplom\data\parsing\final_companies_with_text.xlsx')
 
     df = data[['Wavelength_p', 'Energy_p',
                'Wavelength_f', 'Energy_f', 'Wavelength', 'Energy', 'Price', 'Shipment', 'Total_x', 'Total_y',
-               'Total_medicine', 'In_spie', 'Country', 'CB', 'Public/private']]
+               'Total_medicine', 'In_spie', 'Country', 'CB', 'Public/private', 'aws1', 'aws2', 'aws3', 'aws4',
+               'Mensions', 'Positiveness', 'Average positiveness']]
 
     df['Totalxy'] = df['Total_x'] + df['Total_y']
     df['Country_good'] = np.where(df['Country'].isin(['finland', 'us', 'china', 'germany']), 1, 0)
     df['Shares'] = np.where(df['Public/private'] == 'public', 1, 0)
+    df['aws1'] = np.where(df['aws1'] == 'None', df['aws1'].mean(), df['aws1'])
+    df['aws2'] = np.where(df['aws2'] == -1, df['aws2'].mean(), df['aws2'])
+    df['aws3'] = np.where(df['aws3'] == -1, df['aws3'].mean(), df['aws3'])
+    df['aws4'] = np.where(df['aws4'] == -1, df['aws4'].mean(), df['aws4'])
 
     df.drop(columns=['Total_x', 'Total_y', 'Country', 'Public/private'], inplace=True)
 
@@ -26,6 +31,7 @@ def make_net_scores():
     x_scaled = min_max_scaler.fit_transform(x)
     df = pd.DataFrame(x_scaled, columns=['Wavelength_p', 'Energy_p', 'Wavelength_f', 'Energy_f', 'Wavelength',
                                          'Energy', 'Price', 'Shipment', 'Total_medicine', 'In_spie', 'CB',
+                                         'aws1', 'aws2', 'aws3', 'aws4', 'Mensions', 'Positiveness', 'Average positiveness',
                                          'Totalxy', 'Country_good', 'Shares'])
 
     df['Url'] = data['Url1']
@@ -63,9 +69,23 @@ def make_net_scores():
         BN.bake_network(e1, w1, e2)
         score_org = BN.make_scores()
 
-        data_scores[row['Url']] = [score_product, score_tech, score_org, row['target']]
+        # Consumers Net
+        w1 = row['Average positiveness']
+        e1 = row['Mensions']
+        w2 = row['aws1']
+        e2 = row['Positiveness']
+        w3 = row['aws3']
+        e3 = row['aws2']
+        dt = row['aws4']
 
-    result_df = pd.DataFrame.from_dict(data_scores, orient='index', columns=['product', 'tech', 'org', 'target'])
+        BN = CustomerBayesNet()
+        BN.bake_network(e1, w1, e2, w2, e3, w3, dt)
+        score_customers = BN.make_scores()
+
+        # Make dict
+        data_scores[row['Url']] = [score_product, score_tech, score_org, score_customers, row['target']]
+
+    result_df = pd.DataFrame.from_dict(data_scores, orient='index', columns=['product', 'tech', 'org', 'customers', 'target'])
 
     return result_df
 
